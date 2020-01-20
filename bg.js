8c89f4c87f813f500c833082e8f553af7da4dcaf
kec <57247368+yesky85@users.noreply.github.com>
2020-01-20 15:53:08 +0900

    Modify bgImage Use Unsplash API (image & location)

--- bg.js
+++ bg.js
@@ -1,22 +1,55 @@
-const body = document.querySelector('body');
+const UNSPLASH_API_KEY = `cb58b5b55ecbceaef8215c585cc7ac82768fff400e940ac23c49e7f4ff1d6ae7`;
+const UNSPLASH_API = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
 
-const IMAGE_NUMBER = 3;
+const bgBody = document.querySelector('body');
+const locationContainer = document.getElementsByClassName('js-area');
 
-function paintImage(number) {
-  const image = new Image();
-  image.src = `./images/${number + 1}.jpg`;
-  image.className = 'bgImage';
-  body.appendChild(image);
+function getBackground() {
+  fetch(UNSPLASH_API)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      const image = json;
+      const location = image.location;
+      saveBackground(image.urls.full, location.city, location.country);
+    });
 }
 
-function genRandom() {
-  const number = Math.floor(Math.random() * IMAGE_NUMBER);
-  return number;
+function saveBackground(urls, city, country) {
+  const expiresDate = new Date();
+  expiresDate.setHours(expiresDate.getHours() + 1);
+  const image = {
+    urls,
+    expires: expiresDate,
+    city,
+    country,
+  };
+  localStorage.bgImage = JSON.stringify(image);
+  loadBackground();
+}
+
+function loadBackground() {
+  const loadedImage = localStorage.bgImage;
+  if (loadedImage) {
+    const parsedImage = JSON.parse(loadedImage);
+    const today = new Date();
+    const expires = new Date(parsedImage.expires);
+    console.log(today, expires);
+    if (today > expires) {
+      getBackground();
+    } else {
+      bgBody.style.backgroundImage = `url(${parsedImage.urls})`;
+      bgBody.style.backgroundRepeat = 'no-repeat';
+      locationContainer[0].innerHTML = `${parsedImage.city}, ${parsedImage.country}`;
+    }
+  } else {
+    getBackground();
+  }
 }
 
 function init() {
-  const randomNumber = genRandom();
-  paintImage(randomNumber);
+  loadBackground();
 }
 
 init();
